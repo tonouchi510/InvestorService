65dfa83ce5c84363f51e94bea25973a2d6d1c498
tonouchi510 <tonouchi27@gmail.com>
2021-10-27 23:42:22 +0900

    Add user entity

---
+++ internal/domain/models/users/user.go
@@ -0,0 +1,87 @@
+package users
+
+import (
+	"fmt"
+
+	"github.com/tonouchi510/InvestorService/internal/domain/models/investmentitem"
+)
+
+type User struct {
+	Id                 UserId
+	name               UserName
+	status             BillingStatus
+	investmentItemList []investmentitem.InvestmentItemId
+}
+
+func NewUser(id UserId, name UserName, status BillingStatus, items []investmentitem.InvestmentItemId) (*User, error) {
+	newUser := User{
+		Id:                 id,
+		name:               name,
+		status:             status,
+		investmentItemList: items,
+	}
+	return &newUser, nil
+}
+
+func (u User) Equals(other User) (bool, error) {
+	return (u.Id == other.Id), nil
+}
+
+func (u *User) ChangeName(name UserName) error {
+	u.name = name
+	return nil
+}
+
+func (u *User) ChangeStatus(status BillingStatus) error {
+	u.status = status
+	return nil
+}
+
+func (u *User) AddInvestmentItem(itemId investmentitem.InvestmentItemId) error {
+	if u.IsInvestmentItemLimit() {
+		return fmt.Errorf("InvestmentItem数が既に上限です。(%d件)", len(u.investmentItemList))
+	}
+	if u.IsDuplicatedItemList(itemId) {
+		return fmt.Errorf("InvestmentItemId: %s は既に追加されています。", itemId)
+	}
+	u.investmentItemList = append(u.investmentItemList, itemId)
+	return nil
+}
+
+func (u *User) RemoveInvestmentItem(itemId investmentitem.InvestmentItemId) error {
+	results := []investmentitem.InvestmentItemId{}
+	size := len(u.investmentItemList)
+	for _, v := range u.investmentItemList {
+		if v != itemId {
+			results = append(results, v)
+		}
+	}
+	if len(results) != (size - 1) {
+		// 指定したitemIdが元々ない場合
+		return fmt.Errorf("InvestmentItemId: %s が存在しません。", itemId)
+	}
+	u.investmentItemList = results
+	return nil
+}
+
+func (u User) IsDuplicatedItemList(itemId investmentitem.InvestmentItemId) bool {
+	for _, v := range u.investmentItemList {
+		if v == itemId {
+			return true
+		}
+	}
+	return false
+}
+
+func (u User) IsInvestmentItemLimit() bool {
+	c := len(u.investmentItemList)
+	// 将来的にユーザランクの概念も導入し、ステータスと組み合わせて制限設定するのも良いかも
+	switch u.status {
+	case Free:
+		return c <= 5
+	case Premium:
+		return c <= 30
+	default:
+		return c <= 5
+	}
+}
